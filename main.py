3c2068a9025a2cfbbd2ab360bbb674c0d90617aa
keitamatvijuka <125823796+keitamatvijuka@users.noreply.github.com>
2023-02-19 20:29:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,22 +11,36 @@ def are_matching(left, right):
 
 
 def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-
+    stack = []
+    for i, char in enumerate(text):
+        if char in "([{":
+            # Push opening bracket onto stack
+            stack.append((char, i))
+        elif char in ")]}":
+            if not stack:
+                # If stack is empty, there's a closing bracket with no matching opening bracket
+                return i
+            else:
+                top_char, top_pos = stack.pop()
+                # Check for mismatching brackets
+                if (top_char == "(" and char != ")") or \
+                    (top_char == "[" and char != "]") or \
+                    (top_char == "{" and char != "}"):
+                    return i
+    if stack:
+        # If there are unmatched opening brackets, return their position
+        return stack[0][1]
+    else:
+        # If all brackets match, return "Success"
+        return "Success"
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-
+    if mismatch == "Success":
+        print(mismatch)
+    else:
+        print(mismatch + 1) # Add 1 to convert 0-indexed position to 1-indexed
 
 if __name__ == "__main__":
     main()
