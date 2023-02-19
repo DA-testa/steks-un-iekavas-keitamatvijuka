869ef2e1a2abbcce8923d39b070cfbb9853b5bf0
keitamatvijuka <125823796+keitamatvijuka@users.noreply.github.com>
2023-02-19 20:50:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,33 +13,36 @@ def are_matching(left, right):
 def find_mismatch(text):
     stack = []
     for i, char in enumerate(text):
-        if char in "([{"
+        if char in "([{":
+            
             stack.append((char, i))
         elif char in ")]}":
             if not stack:
-  
+             
                 return i
             else:
                 top_char, top_pos = stack.pop()
-                # Check for mismatching brackets
+                
                 if (top_char == "(" and char != ")") or \
                     (top_char == "[" and char != "]") or \
                     (top_char == "{" and char != "}"):
                     return i
     if stack:
-
+ 
         return stack[0][1]
     else:
-
+      
         return "Success"
 
 def main():
     text = input()
-    mismatch = find_mismatch(text)
-    if mismatch == "Success":
-        print(mismatch)
-    else:
-        print(mismatch + 1) 
+    if "I" in text:
+        text = input()
+        mismatch = find_mismatch(text)
+        if mismatch == "Success":
+            print(mismatch)
+        else:
+            print(mismatch + 1) 
 
 if __name__ == "__main__":
     main()
